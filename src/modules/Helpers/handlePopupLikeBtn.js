4810edc30fffb41b4d157f7bf52a2e0cbc37e889
Kweeka <mtalbi29@yahoo.com>
2022-11-24 21:55:31 +0100

    Fix linter errors

--- src/modules/Helpers/handlePopupLikeBtn.js
+++ src/modules/Helpers/handlePopupLikeBtn.js
@@ -1,10 +1,10 @@
-import displayLikes from "../AppComponents/displayLikes";
-import { AddLike, getLikes } from "../InvolvementService/LikeService";
+import displayLikes from '../AppComponents/displayLikes.js';
+import { AddLike, getLikes } from '../InvolvementService/LikeService.js';
 
 const handlePopupLikeBtn = async (movieId) => {
   await AddLike(movieId);
   const showLikes = await getLikes();
   displayLikes(showLikes);
-}
+};
 
 export default handlePopupLikeBtn;
