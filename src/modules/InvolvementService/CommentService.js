76257543efbf54ac49d9b17d50bb8d2dba71e455
Mohamed Talbi <mtalbi29@yahoo.com>
2022-11-25 18:57:22 +0100

    Update CommentService.js

--- src/modules/InvolvementService/CommentService.js
+++ src/modules/InvolvementService/CommentService.js
@@ -2,7 +2,7 @@ const BASE_ROUTE = 'https://us-central1-involvement-api.cloudfunctions.net/capst
 
 export const getComments = async (movieId) => {
   try {
-    const response = await fetch(`${BASE_ROUTE}/${process.env.INVOLVEMENT_KEY || '8o1u2AYOtlORRdJgAQd1'}/comments?item_id=${movieId.replace('popup-', '')}`);
+    const response = await fetch(`${BASE_ROUTE}/${process.env.INVOLVEMENT_KEY || '8o1u2AYOtlORRdJgAQd1'}/comments?item_id=${movieId}`);
 
     if (!response.ok) return [];
     return response.json();
